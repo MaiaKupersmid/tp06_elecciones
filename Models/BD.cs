44725d45247d55ea70f826db5ebe832193b87cf3
JULIETA IZBICKI <47350196@ort>
2023-07-05 11:59:57 -0300

    nada

--- Models/BD.cs
+++ Models/BD.cs
@@ -1,16 +1,22 @@
 using System.Data.SqlClient;
 using Dapper;
-
+//namesapce
 public static class BD
 {
     public static string _connectionString = @"Server=localhost;
     Database=Elecciones2023;Trusted_Connection=True;";
 
-    /*
+    
     public static void AgregarCandidato(Candidato can)
     {
-
+        using(SqlConnection BD = new SqlConnection(_connectionString))
+        {
+            string sql= "INSERT INTO Candidato(Apellido, Nombre, FechaNacimiento) VALUES(Kuper, Maiu, 08/06/2007)";
+            =BD.Query<,>(sql).ToList();
+        }
     }
+    /*
+
     public static void EliminarCandidato(int idCandidato)
     {
 
@@ -21,7 +27,7 @@ public static class BD
     }
     public static Candidato VerInfoCandidato(int idCandidato)
     {
-       // return ;
+        return ;
     }
     public static Partido ListarPartidos()
     {
