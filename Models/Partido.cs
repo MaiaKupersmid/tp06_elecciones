17f0171521c4ffeb1bc81ee22a7450a4b11cd054
Jules <129423966+julietaIzbicki@users.noreply.github.com>
2023-07-18 18:40:46 -0300

    partido tiene contructor.cs

--- Models/Partido.cs
+++ Models/Partido.cs
@@ -7,4 +7,13 @@ public class Partido
     public DateTime FechaFundacion {get; set;}
     public int CantidadDiputados {get; set;}
     public int CantidadSenadores {get; set;}
+    public Partido(string nombre, string logo, string sitioWeb, DateTime fechaFundacion, int cantidadDiputados, int cantidadSenadores){
+        Nombre = nombre;
+        Logo = logo;
+        SitioWeb = sitioWeb;
+        FechaFundacion = fechaFundacion; 
+        CantidadDiputados = cantidadDiputados;
+        CantidadSenadores = cantidadSenadores;
+    }
+
 }
